7132b74629341ca8927595bffb5c84c02323df84
hiramatsu <hiramatsu3300@gmail.com>
2019-02-20 14:17:56 +0900

    baseUrl

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
     outputDir: 'docs',
     assetsDir: './',
     publicPath: './'
+    baseUrl: process.env.NODE_ENV === 'production'
+      ? './sample-project/'
+      : '/'
 }
 
 
